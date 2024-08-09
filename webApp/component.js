579cab3e01a1ddcdb67c0112f996424aaa7802f5
Núbia Tirabassi <nu.ragagnin91@gmail.com>
2024-08-09 11:34:54 -0300

    implementação Step 30: Routing and Navigation

--- webApp/component.js
+++ webApp/component.js
@@ -1,26 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
- ], (UIComponent, JSONModel) => {
-    "use strict";
- 
-    return UIComponent.extend("ui5.walkthrough.Component", {
-       metadata : {
-          interfaces: ["sap.ui.core.IAsyncContentCreation"],
-          manifest: "json"
-       },
- 
-       init() {
-          UIComponent.prototype.init.apply(this, arguments);
- 
-          const oData = {
-             recipient : {
-                name : "World"
-             }
-          };
-          const oModel = new JSONModel(oData);
-          this.setModel(oModel);
-       }
-    });
- });
- 
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], (UIComponent, JSONModel) => {
+	"use strict";
+
+	return UIComponent.extend("ui5.walkthrough.Component", {
+
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+
+		init() {
+			// chama a função init da classe pai
+			UIComponent.prototype.init.apply(this, arguments);
+
+			const oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			const oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// inicializador da URL/hash
+			this.getRouter().initialize();
+		}
+	});
+});
